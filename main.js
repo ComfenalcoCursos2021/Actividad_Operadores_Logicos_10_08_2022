f08154482adc54ab6d74c6fb59239befa1a96890
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-08-10 21:27:35 -0500

    Maquetacion terminada

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ addEventListener("DOMContentLoaded", async(e)=>{
     let peticion = await fetch("api.php");
     let data = await peticion.text();
     document.querySelector("#res").innerHTML = data;
+    
+    // let td = document.querySelectorAll("tbody tr td");
+    // td.forEach(element => {
+    //     element.dataset.numero = (parseInt(element.dataset.numero)) ? "true" : "false"; 
+    // });
 })
